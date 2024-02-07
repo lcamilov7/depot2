fa7cfbfd856a6ff452e7c9e6a848ea38949a6134
Luis Camilo <luiscamilov@hotmail.com>
2024-02-07 16:55:04 -0500

    mejorando css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
@@ -7,17 +9,56 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to product_url(@product), notice: "Product was succesfully created." }
+        format.json { render :show, status: :created, location: @product }
+      else
+        puts @product.errors.full_message
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to product_url(@product), notice: "The product was succesfully updated." }
+        format.json { render :show, status: :unprocessable_entity }
+      else
+        puts @product.errors.full_message
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @product.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "The product was succesfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
   end
 end
