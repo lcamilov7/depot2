1305b223d3419e3bebe155742102046dff7b5811
Luis Camilo <luiscamilov@hotmail.com>
2024-02-09 10:58:01 -0500

    aplicamos cache al render de index de store

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,12 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+
+    # Test para contenido usando selectores CSS
+    assert_select "li a", minimum: 4
+    assert_select 'header img', minimum: 1
+    assert_select 'main ul li', 3
+    assert_select 'h2', 'MyString'
+    assert_select 'div', /\$[,\d]+\.\d\d/     # Price
   end
 end
