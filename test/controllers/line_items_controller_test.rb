f69e67e11b1a73daae0ac0a2b03ff5897295b743
Luis Camilo <luiscamilov@hotmail.com>
2024-02-10 12:24:57 -0500

    antes de intentar session counter

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,13 +1,14 @@
 require "test_helper"
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get line_items_create_url
-    assert_response :success
-  end
+  test "should create line item" do
+    assert_difference("LineItem.count") do
+      puts products(:ruby).id
+      post product_line_items_url, params: { product_id: products(:ruby).id }
+    end
 
-  test "should get destroy" do
-    get line_items_destroy_url
-    assert_response :success
+    follow_redirect!
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'p', 'Programming Ruby 1.9'
   end
 end
