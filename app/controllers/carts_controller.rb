4adbb7640df2c7cf119567b0a1d541eb654a6e64
Luis Camilo <luiscamilov@hotmail.com>
2024-02-10 14:48:00 -0500

    añadi un error si tratan de introducir un id para la ruta carts/:id que es invalido

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,14 @@
 class CartsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
   def show
     @cart = Cart.find(params[:id])
+  end
+
+  private
+
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to root_path, notice: "Error: Invalid cart"
   end
 end
