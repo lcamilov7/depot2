0ade06f31f0bf59ee74b13291760b33f38205e05
Luis Camilo <luiscamilov@hotmail.com>
2024-02-10 13:24:09 -0500

    agregamos quantity a line item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    # Tiene que ser find_by porque el find no devuelve ni false ni nil
+    current_item = LineItem.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = LineItem.new(cart: self, product: product)
+    end
+    return current_item
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_163729) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_10_180434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_163729) do
     t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
